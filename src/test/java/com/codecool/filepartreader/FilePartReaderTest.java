c3671b4a91dec6a004ea694b302d4f3a5c194c51
Jasio <mcyanek@gmail.com>
2020-09-22 20:05:20 +0200

    adds test file, and empty tests

---
+++ src/test/java/com/codecool/filepartreader/FilePartReaderTest.java
@@ -0,0 +1,30 @@
+package com.codecool.filepartreader;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FilePartReaderTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void setup() {
+    }
+
+    @Test
+    public void read() {
+    }
+
+    @Test
+    public void readLines() {
+    }
+}
---
+++ src/test/java/com/codecool/filepartreader/FileWordAnalyzerTest.java
@@ -0,0 +1,30 @@
+package com.codecool.filepartreader;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FileWordAnalyzerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getWordsOrderedAlphabetically() {
+    }
+
+    @Test
+    public void getWordsContainingSubstring() {
+    }
+
+    @Test
+    public void getStringsWhichPalindromes() {
+    }
+}
